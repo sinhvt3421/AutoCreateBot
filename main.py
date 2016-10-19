4ce3f9ef7c319b7942263b93b0a9a3c5dc59f17a
SINH-PC\Admin <sinh3497@gmail.com>
2016-10-19 10:06:53 +0700

    First up for repo

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import setEnglish
+import details
+import BotDB
+from BotInfo import BotInfo
+
+"""driver = webdriver.Firefox()
+driver.maximize_window()
+driver.get("http://wwww.facebook.com")
+
+email =  driver.find_element_by_id("email")
+passw = driver.find_element_by_id("pass")
+email.send_keys("hoangminh.le12302@gmail.com")
+passw.send_keys("hoangminh12302")
+time.sleep(0.5)
+btn = driver.find_element_by_id("u_0_l")
+btn.click()
+time.sleep(0.5)
+#setEnglish.setting(driver)
+name =  driver.find_element_by_class_name("_2s25")
+img = driver.find_element_by_xpath(".//img").get_attribute("id")
+id = img.split('_')[3]
+print(id)
+name.click()
+driver.get("www.facebook.com/" + id + "/" + "about")
+details.Overview(driver)"""

dacc8d1dbc2ede966c9bc480ff34e4119f63deb4
vts3497 <sinh3497@gmail.com>
2016-10-20 16:49:30 +0700

    config path

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import time
 import DataInterac
 import Settings
 import MakeBotProfile
+import ConfigParser
 
-driver = webdriver.Firefox()
+"""driver = webdriver.Firefox()
 driver.maximize_window()
 driver.get("http://wwww.facebook.com")
 email =  driver.find_element_by_id("email")
@@ -17,6 +18,7 @@ btn = driver.find_element_by_id("u_0_l")
 btn.click()
 time.sleep(0.5)
 #setEnglish.setting(driver)
+"""
 """name =  driver.find_element_by_class_name("_2s25")
 img = driver.find_element_by_xpath(".//img").get_attribute("id")
 id = img.split('_')[3]
@@ -25,4 +27,10 @@ name.click()
 driver.get("www.facebook.com/" + id + "/" + "about")
 details.Overview(driver)"""
 #sendFriendRq.sendRequest(driver)
-sendFriendRq.likeFanpage(driver)
+#sendFriendRq.likeFanpage(driver)
+config = ConfigParser.SafeConfigParser()
+config.read('config.ini')
+name = config.get('folder_path','name')
+img = config.get('folder_path','img')
+print(name)
+print(img)
